f90539c78f5af29ae48a15bd991c2f3377489de1
zcipod <zhaochang168@gmail.com>
2020-10-30 12:47:07 +1100

    add route

---
+++ route.tf
@@ -0,0 +1,7 @@
+resource "google_compute_route" "route" {
+  count = var.WORKER_NUM
+  name         = "kubernetes-route-10-200-${count.index}-0-24"
+  network      = google_compute_network.vpc_network.name
+  dest_range   = "10.200.${count.index}.0/24"
+  next_hop_ip = "10.240.0.2${count.index}"
+}
