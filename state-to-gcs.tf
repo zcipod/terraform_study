852ff57f3fbfb301b8404af505a0868adfca560a
zcipod <zhaochang168@gmail.com>
2020-11-02 12:40:47 +1100

    save state file to gcs bucket

---
+++ state-to-gcs.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-zcipod"
+    prefix  = "terraform/state"
+  }
+}
+
+data "terraform_remote_state" "gcs-bucket" {
+  backend = "gcs"
+  config = {
+    bucket  = "terraform-state"
+    prefix  = "prod"
+  }
+}
+
+resource "template_file" "template" {
+  template = data.terraform_remote_state.gcs-bucket.greeting
+}
